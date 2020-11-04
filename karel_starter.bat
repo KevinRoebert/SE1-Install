def7c5c293aba63b9a0dc19ddef110e10e14d136
Kevin R <dev@roebert.eu>
2020-11-04 21:08:39 +0100

    Support Java > 8

--- karel_starter.bat
+++ karel_starter.bat
@@ -3,11 +3,17 @@
 if "%JAVA_HOME%" == "" (
     echo "JAVA_HOME is not defined"
     for /f %%j in ("java.exe") do (
-		for /f tokens^=2-5^ delims^=.-_^" %%v in ('java -fullversion 2^>^&1') do (		
-			if %%v LSS 8 (
-				echo "Java version is too low"
+		for /f tokens^=2-3^ delims^=.-_^" %%v in ('java -fullversion 2^>^&1') do (
+			set version = false
+			
+			if %%v GEQ 8 set version=true
+			if %%w GEQ 8 set version=true
+		
+			if "%version%" == "true" (
+				echo "Java version (%%v.%%w) is too low"
 				echo "at least 1.8 is needed"
 			) else (
+				echo "Found Java in version %%v.%%w at: %%~dp$PATH:j"
 				"%%~dp$PATH:j\java.exe" -jar karel2020.jar
 				goto :continue
 			)
